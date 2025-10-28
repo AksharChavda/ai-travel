eceb1ffc9c07fc6703e93c28e449e2a31b6eedff
Akshar-Chavda <akshar.inbweb25@gmail.com>
2025-10-28 23:20:21 +0530

    any

--- src/view-trip/components/placeToVisit.jsx
+++ src/view-trip/components/placeToVisit.jsx
@@ -15,7 +15,7 @@ function PlacesToVisit({ trip }) {
           {day.plan?.length > 0 ? (
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {day.plan.map((place, idx) => (
-                <PlaceCardItem key={idx} place={place} />
+                <PlaceCardItem place={place} />
               ))}
             </div>
           ) : (
